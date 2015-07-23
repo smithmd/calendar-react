a32a299359de7a436510e7cc876419b737c4f19f
Michael Smith <smithmd@interlochen.org>
2015-07-23 15:45:47 -0400

    adding ReactiveX.js

--- build/data.js
+++ build/data.js
@@ -1,3 +1,4 @@
 /**
  * Created by smithmd on 7/23/15.
  */
+var selectedDate = Observable([]);
---
+++ js/data.js
@@ -0,0 +1,8 @@
+/**
+ * Created by smithmd on 7/22/15.
+ */
+
+var weekdays = ['Sunday', 'Monday', 'Tuesday', 'Wednesday', 'Thursday', 'Friday', 'Saturday'];
+var months = ['January', 'February', 'March', 'April', 'May', 'June', 'July', 'August', 'September', 'October', 'November', 'December'];
+
+var selectedDate = Observable([]);
